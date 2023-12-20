5e2e72cd433fc7cc141f800b788de5862612ad81
giugallotti <giulianagallotti@gmail.com>
2023-12-20 20:17:28 -0300

    agrego punto 2

--- logico.pl
+++ logico.pl
@@ -12,16 +12,29 @@ nro(6).
 nro(7).
 nro(8).
 
-
 %punto 1 - casilleros libres
 casilleroLibre(Fil, Col) :-
+    nro(Fil),
+    nro(Col),
     not(pieza(_, _, Fil, Col)).
 
 %punto 2
 %casillero ocupado por 2 piezas a la vez
 dosPiezasEnElMismoLugar(Fil, Col) :-
-    pieza(Pieza1, _, Fil, Col),
-    pieza(Pieza2, _, Fil, Col),
+    pieza(Pieza1, Color, Fil, Col),
+    pieza(Pieza2, Color, Fil, Col),
     Pieza1 \= Pieza2.
 
-%
+dosPiezasEnElMismoLugar(Fil, Col) :-
+    pieza(Pieza1, Color1, Fil, Col),
+    pieza(Pieza2, Color2, Fil, Col),
+    Pieza1 \= Pieza2,
+    Color1 \= Color2.
+
+%pieza en casillero invalido
+%es invalido cuando esta en uno ocupado o se va del rango de fila o columna
+casilleroInvalido(Fil, Col) :-
+    not(nro(Fil)),
+    not(nro(Col)),
+    pieza(_,_,Fil,Col).
+
